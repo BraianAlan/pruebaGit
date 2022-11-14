fcf3fef7fdda34cc8e4c4469d48f8aaf58c1538c
Braian Briseña <braian171094@hotmail.com>
2022-11-14 19:45:14 -0300

    Agrega funcion sumar

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 let saludo = "Hola";
+
+/* git log = muestra los archivos subidos
+commit fdb08d0d47b7d37d45526a9ff32dd6aff5c42ee2 (HEAD -> master)
+Author: Braian Briseña <braian171094@hotmail.com>
+Date:   Mon Nov 14 19:36:19 2022 -0300
+
+    Agrega archivo config */
